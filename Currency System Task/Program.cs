cc7b69dedc6f8ddebdf8fe85508dc040d339893c
testtttttttttt1234 <wapigix358@bsomek.com>
2024-05-20 18:31:14 +0400

    Added

--- Currency System Task/Program.cs
+++ Currency System Task/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace Currency_System_Task
+﻿namespace CurrencyManagemenntSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string[] currencyCodes = { "USD", "RUB", "TRY" };
+            decimal[] currencyRates = { 1.70M, 0.0187M, 0.0527M };
+            int index = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Movcud operasiyalar: ");
+                Console.WriteLine("1.Mezennelerin kodları ve kurs qiymetleri ile birlikde gösterilmesi");
+                Console.WriteLine("2.Spesifik bir mezennenin kodu ve kurs qiymeti ile birlikde gösterilmesi");
+                Console.WriteLine("3.Verilmiş mezzenneye ve qiymete uyğun olaraq mebleğin hesablanması");
+                Console.WriteLine("4.Programdan çıxmaq ");
+                Console.Write("Operasiyani daxil edin: ");
+
+                string operation = Console.ReadLine();
+
+                if (operation == "1")
+                {
+                    while (index < currencyCodes.Length)
+                    {
+                        Console.WriteLine(currencyRates[index] + currencyCodes[index]);
+                        index++;
+                    }
+                    index = 0;
+                }
+
+            }
         }
     }
 }
